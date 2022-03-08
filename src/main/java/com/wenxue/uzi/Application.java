7aaf4b0b1a31fc89083e35cd3f972153cf96e1ab
wangkun <wangkun@yl.com>
2022-03-08 17:10:10 +0800

    add build

--- src/main/java/com/wenxue/uzi/Application.java
+++ src/main/java/com/wenxue/uzi/Application.java
@@ -2,6 +2,7 @@ package com.wenxue.uzi;
 
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @EnableApolloConfig(value = {"common.bootstrap","application","dbConnection.yml","common.mybatis-plus"})
+@MapperScan({"com.wenxue.uzi.mapper"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
